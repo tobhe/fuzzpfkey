bae1ca5f1c348d73ad4f241cd006f88eb6abacc3
Tobias Heider <me@tobhe.de>
2023-10-12 12:49:57 +0200

    Rewrite based on the afl_proxy example in AFLPlusPlus

--- fuzz/fuzzpfkey.c
+++ fuzz/fuzzpfkey.c
@@ -1,32 +1,31 @@
 /*
- * Copyright (c) 2023 Tobias Heider <me@tobhe.de>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+   Copyright 2023 Tobias Heider <tobhe@openbsd.org>
+   Copyright 2019-2023 AFLplusplus Project. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+   http://www.apache.org/licenses/LICENSE-2.0
+*/
 
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <signal.h>
 #include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
+#include <assert.h>
+#include <stdint.h>
+#include <errno.h>
 
-#include <sys/types.h>
-#include <sys/shm.h>
-#include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 
 #ifdef __OpenBSD__
 #include <net/pfkeyv2.h>
@@ -40,10 +39,34 @@
 
 #include "siphash.h"
 
+/* AFL config*/
+#define MAP_SIZE_POW2 16
+#define MAP_SIZE (1U << MAP_SIZE_POW2)
+
+#define FORKSRV_FD 198
+
+#define SHM_ENV_VAR "__AFL_SHM_ID"
+
+#define FS_OPT_ERROR 0xf800008f
+#define FS_OPT_GET_ERROR(x) ((x & 0x00ffff00) >> 8)
+#define FS_OPT_SET_ERROR(x) ((x & 0x0000ffff) << 8)
+#define FS_ERROR_MAP_SIZE 1
+#define FS_ERROR_SHMAT 8
+
+#define FS_OPT_ENABLED 0x80000001
+#define FS_OPT_MAPSIZE 0x40000000
+#define FS_OPT_MAX_MAPSIZE ((0x00fffffeU >> 1) + 1)
+#define FS_OPT_SET_MAPSIZE(x) \
+  (x <= 1 || x > FS_OPT_MAX_MAPSIZE ? 0 : ((x - 1) << 1))
+
+uint8_t *__afl_area_ptr;
+
+__thread uint32_t __afl_map_size = MAP_SIZE;
+
 struct kcov {
-	int		 fd;
-	unsigned long	 size;
-	unsigned long	*cover;
+	int		 k_fd;
+	unsigned long	 k_size;
+	unsigned long	*k_cover;
 };
 
 struct kcov *
@@ -76,9 +99,9 @@ kcov_new(void)
 	}
 
 	k = calloc(1, sizeof(struct kcov));
-	k->fd = fd;
-	k->cover = cover;
-	k->size = size;
+	k->k_fd = fd;
+	k->k_cover = cover;
+	k->k_size = size;
 	return k;
 }
 
@@ -86,28 +109,28 @@ void
 kcov_enable(struct kcov *kcov)
 {
 	/* reset counter */
-	__atomic_store_n(&kcov->cover[0], 0, __ATOMIC_RELAXED);
+	__atomic_store_n(&kcov->k_cover[0], 0, __ATOMIC_RELAXED);
 
 #if defined(__OpenBSD__)
 	int mode = KCOV_MODE_TRACE_PC;
-	if (ioctl(kcov->fd, KIOENABLE, &mode) != 0)
+	if (ioctl(kcov->k_fd, KIOENABLE, &mode) != 0)
 		err(1, "ioctl(KIOENABLE)");
 #elif defined(__linux__)
-	if (ioctl(kcov->fd, KCOV_ENABLE, KCOV_TRACE_PC) != 0)
+	if (ioctl(kcov->k_fd, KCOV_ENABLE, KCOV_TRACE_PC) != 0)
 		err(1, "ioctl(KCOV_ENABLE)");
 #endif
 }
 
 int kcov_disable(struct kcov *kcov)
 {
-	int kcov_len = __atomic_load_n(&kcov->cover[0], __ATOMIC_RELAXED);
+	int kcov_len = __atomic_load_n(&kcov->k_cover[0], __ATOMIC_RELAXED);
 
 	/* Stop actual couting. */
 #if defined(__OpenBSD__)
-	if (ioctl(kcov->fd, KIODISABLE) != 0)
+	if (ioctl(kcov->k_fd, KIODISABLE) != 0)
 		err(1, "ioctl(KIODISABLE)");
 #elif defined(__linux__)
-	if (ioctl(kcov->fd, KCOV_DISABLE, 0) != 0)
+	if (ioctl(kcov->k_fd, KCOV_DISABLE, 0) != 0)
 		err(1, "ioctl(KCOV_DISABLE)");
 #endif
 
@@ -116,16 +139,171 @@ int kcov_disable(struct kcov *kcov)
 
 void kcov_free(struct kcov *kcov)
 {
-	close(kcov->fd);
-	kcov->fd = -1;
-	munmap(kcov->cover, kcov->size * sizeof(unsigned long));
-	kcov->cover = MAP_FAILED;
+	close(kcov->k_fd);
+	kcov->k_fd = -1;
+	munmap(kcov->k_cover, kcov->k_size * sizeof(unsigned long));
+	kcov->k_cover = MAP_FAILED;
 }
 
 unsigned long *kcov_cover(struct kcov *kcov)
 {
-	return kcov->cover;
+	return kcov->k_cover;
 }
+
+
+/* Error reporting to forkserver controller */
+
+void send_forkserver_error(int error) {
+
+  uint32_t status;
+  if (!error || error > 0xffff) return;
+  status = (FS_OPT_ERROR | FS_OPT_SET_ERROR(error));
+  if (write(FORKSRV_FD + 1, (char *)&status, 4) != 4) return;
+
+}
+
+/* SHM setup. */
+
+static void __afl_map_shm(void) {
+
+  char *id_str = getenv(SHM_ENV_VAR);
+  char *ptr;
+
+  /* NOTE TODO BUG FIXME: if you want to supply a variable sized map then
+     uncomment the following: */
+
+  /*
+  if ((ptr = getenv("AFL_MAP_SIZE")) != NULL) {
+
+    uint32_t val = atoi(ptr);
+    if (val > 0) __afl_map_size = val;
+
+  }
+
+  */
+
+  if (__afl_map_size > MAP_SIZE) {
+
+    if (__afl_map_size > FS_OPT_MAX_MAPSIZE) {
+
+      fprintf(stderr,
+              "Error: AFL++ tools *require* to set AFL_MAP_SIZE to %u to "
+              "be able to run this instrumented program!\n",
+              __afl_map_size);
+      if (id_str) {
+
+        send_forkserver_error(FS_ERROR_MAP_SIZE);
+        exit(-1);
+
+      }
+
+    } else {
+
+      fprintf(stderr,
+              "Warning: AFL++ tools will need to set AFL_MAP_SIZE to %u to "
+              "be able to run this instrumented program!\n",
+              __afl_map_size);
+
+    }
+
+  }
+
+  if (id_str) {
+
+#ifdef USEMMAP
+    const char    *shm_file_path = id_str;
+    int            shm_fd = -1;
+    unsigned char *shm_base = NULL;
+
+    /* create the shared memory segment as if it was a file */
+    shm_fd = shm_open(shm_file_path, O_RDWR, 0600);
+    if (shm_fd == -1) {
+
+      fprintf(stderr, "shm_open() failed\n");
+      send_forkserver_error(FS_ERROR_SHM_OPEN);
+      exit(1);
+
+    }
+
+    /* map the shared memory segment to the address space of the process */
+    shm_base =
+        mmap(0, __afl_map_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    if (shm_base == MAP_FAILED) {
+
+      close(shm_fd);
+      shm_fd = -1;
+
+      fprintf(stderr, "mmap() failed\n");
+      send_forkserver_error(FS_ERROR_MMAP);
+      exit(2);
+
+    }
+
+    __afl_area_ptr = shm_base;
+#else
+    uint32_t shm_id = atoi(id_str);
+
+    __afl_area_ptr = shmat(shm_id, 0, 0);
+
+#endif
+
+    if (__afl_area_ptr == (void *)-1) {
+
+      send_forkserver_error(FS_ERROR_SHMAT);
+      exit(1);
+
+    }
+
+    /* Write something into the bitmap so that the parent doesn't give up */
+
+    __afl_area_ptr[0] = 1;
+
+  }
+
+}
+
+/* Fork server logic. */
+
+static void __afl_start_forkserver(void) {
+
+  uint8_t  tmp[4] = {0, 0, 0, 0};
+  uint32_t status = 0;
+
+  if (__afl_map_size <= FS_OPT_MAX_MAPSIZE)
+    status |= (FS_OPT_SET_MAPSIZE(__afl_map_size) | FS_OPT_MAPSIZE);
+  if (status) status |= (FS_OPT_ENABLED);
+  memcpy(tmp, &status, 4);
+
+  /* Phone home and tell the parent that we're OK. */
+
+  if (write(FORKSRV_FD + 1, tmp, 4) != 4) return;
+
+}
+
+static uint32_t __afl_next_testcase(uint8_t *buf, uint32_t max_len) {
+
+  int32_t status, res = 0xffffff;
+
+  /* Wait for parent by reading from the pipe. Abort if read fails. */
+  if (read(FORKSRV_FD, &status, 4) != 4) return 0;
+
+  /* we have a testcase - read it */
+  status = read(0, buf, max_len);
+
+  /* report that we are starting the target */
+  if (write(FORKSRV_FD + 1, &res, 4) != 4) return 0;
+
+  return status;
+
+}
+
+static void __afl_end_testcase(void) {
+  int status = 0xffffff;
+  if (write(FORKSRV_FD + 1, &status, 4) != 4) exit(1);
+}
+
+/* you just need to modify the while() loop in this main() */
 
 void
 print_hex(uint8_t *buf, size_t len)
@@ -145,99 +323,69 @@ print_hex(uint8_t *buf, size_t len)
 }
 
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
 	struct kcov	*k;
 	unsigned long	*kbuf;
-	uint8_t		*afl_shared = NULL;
-	const char	*afl_shm_id_str;
-	uint64_t	 previous;
-	int		 sock, kcov_len;
-	uint8_t		 inbuf[512*1024] = {0} ;
-	size_t		 inlen, slen;
-	const char	*path = NULL;
-	int		 i, fd;
+	uint8_t  buf[1024];
+	int len;
+	uint64_t	 previous = 0;
+	int sock, kcov_len;
+	size_t slen;
+	int i;
 
-	if (argc == 2)
-		path = argv[1];
+	__afl_map_size = MAP_SIZE;
+
+	__afl_map_shm();
+	__afl_start_forkserver();
 
 	k = kcov_new();
-	if (k == NULL)
+	if (k == NULL) {
 		errx(1, "kcov_new");
-
-	kbuf = kcov_cover(k);
-
-	afl_shm_id_str = getenv("__AFL_SHM_ID");
-	if (afl_shm_id_str != NULL) {
-		int afl_shm_id = atoi(afl_shm_id_str);
-		afl_shared = shmat(afl_shm_id, NULL, 0);
+		exit(1);
 	}
-	printf("afl_shared: %p\n", afl_shared);
-
-	kcov_enable(k);
 
 	sock = socket(PF_KEY, SOCK_RAW, PF_KEY_V2);
 	if (sock == -1) {
 		err(1, "sock: ");
-		goto done;
+		exit(1);
 	}
 
-	if (path != NULL) {
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-			err(1, "open()");
-	} else {
-		fd = 0;
-	}
+	kbuf = kcov_cover(k);
 
-	/* get input */
-	inlen = read(fd, inbuf, sizeof(inbuf));
+	while ((len = __afl_next_testcase(buf, sizeof(buf))) > 0) {
+		if (len >= sizeof(struct sadb_msg)) {
+			kcov_enable(k);
 
-	/* OpenBSD expects a valid PID */
-	struct sadb_msg *msg = (struct sadb_msg *)inbuf;
-	msg->sadb_msg_version = PF_KEY_V2;
-	msg->sadb_msg_pid = getpid();
-	printf("pid = %x\n", msg->sadb_msg_pid);
-	msg->sadb_msg_len = inlen / 8;
+			struct sadb_msg *msg = (struct sadb_msg *)buf;
+			msg->sadb_msg_version = PF_KEY_V2;
+			msg->sadb_msg_pid = getpid();
+			msg->sadb_msg_len = len / 8;
 
-	printf("sending: len=%zu: ", inlen);
-	print_hex(inbuf, inlen);
-	slen = write(sock, inbuf, inlen);
-	if (slen == -1) {
-		err(1, "write():");
-		goto done;
-	}
+#ifdef DEBUG
+			printf("sending: len=%d: ", len);
+			print_hex(buf, len);
+#endif
+			write(sock, buf, len);
 
-	inlen = read(fd, inbuf, sizeof(inbuf));
-	if (inlen == -1) {
-		err(1, "read(): ");
-		goto done;
-	}
-	printf("recv: len=%zu: ", inlen);
-	print_hex(inbuf, inlen);
- done:
+			kcov_len = kcov_disable(k);
 
-	kcov_len = kcov_disable(k);
+			for (i = 0; i < kcov_len; i++) {
+				uint64_t current = kbuf[i + 1];
+				uint32_t hash = hsiphash_static(&current, sizeof(unsigned long));
+				uint64_t mixed = (hash & 0xffff) ^ previous;
+				previous = (hash & 0xffff) >> 1;
 
-	/* write output */
-	previous = 0;
-	if (afl_shared != NULL) {
-		for (i = 0; i < kcov_len; i++) {
-			uint64_t current = kbuf[i + 1];
-			uint64_t hash = hsiphash_static(&current,
-							sizeof(unsigned long));
-			uint64_t mixed = (hash & 0xffff) ^ previous;
-			previous = (hash & 0xffff) >> 1;
-
-			uint8_t *s = &afl_shared[mixed];
-			int r = __builtin_add_overflow(*s, 1, s);
-			if (r) {
-				*s = 128;
+				uint8_t *s = &__afl_area_ptr[mixed];
+				int r = __builtin_add_overflow(*s, 1, s);
+				if (r) {
+					*s = 128;
+				}
 			}
 		}
+ skip:
+		__afl_end_testcase();
 	}
-
-	kcov_free(k);
-
-	return (0);
+	close(sock);
+	return 0;
 }
